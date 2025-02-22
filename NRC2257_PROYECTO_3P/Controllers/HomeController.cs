c0308a0755d58e0c622af3a82e84f6cf1074c45d
JulioViche <jeviche@espe.edu.ec>
2025-02-22 13:21:10 -0500

    Agregar archivos de proyecto.

---
+++ NRC2257_PROYECTO_3P/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using NRC2257_PROYECTO_3P.Models;
+
+namespace NRC2257_PROYECTO_3P.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
